86c5cafcc94b51e72405a7277e5dda7e4df4780f
williamjuan027 <williamjuan027@gmail.com>
2018-10-08 15:47:15 -0400

    use data from json

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { Page } from "ui/page";
 import { getNumber } from "tns-core-modules/application-settings";
+import { FileReaderService } from "../core/fileReader.service";
 
 @Component({
     selector: "Home",
@@ -11,47 +12,31 @@ import { getNumber } from "tns-core-modules/application-settings";
 })
 export class HomeComponent implements OnInit {
 
-    categories: any[] = [
-        {
-            title: "PROGRAMMING", quizDate: "20 October", lastScore: "70%", backgroundColor: "#250046", image: "~/images/programming.png",
-            questions: [
-                { question: 'Programming question 1', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 0 },
-                { question: 'Programming question 2', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 1 },
-                { question: 'Programming question 3', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 2 },
-                { question: 'Programming question 4', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 3 },
-                { question: 'Programming question 5', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 2 }
-            ]
-        },
-        {
-            title: "NETWORKING", quizDate: "3 December", lastScore: "10%", backgroundColor: "#0D0735", image: "~/images/networking.png",
-            questions: [
-                { question: 'Networking question 1', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 0 },
-                { question: 'Networking question 2', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 1 },
-                { question: 'Networking question 3', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 2 }
-            ]
-        },
-        {
-            title: "SECURITY", footer: "10", headerText: "First", footerText: "4", quizDate: "12 December", lastScore: "100%", backgroundColor: "#020046", image: "~/images/security.png",
-            questions: [
-                { question: 'Security question 1', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 0 },
-                { question: 'Security question 2', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 1 },
-                { question: 'Security question 3', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 2 },
-                { question: 'Security question 4', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 3 },
-                { question: 'Security question 5', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 1 },
-                { question: 'Security question 6', options: ['one', 'two', 'three', 'four'], correctAnswerIndex: 2 },
-            ]
-        }
-    ]
+    categories: any[] = [];
 
     constructor(
         private routerExtensions: RouterExtensions,
-        private page: Page
+        private page: Page,
+        private fileReader: FileReaderService
     ) {
         this.page.actionBarHidden = true;
     }
 
     ngOnInit(): void {
-        this.initializeScore();
+        this.getCategories();
+    }
+
+    getCategories() {
+        this.fileReader.readJSON('/core/questions.json').then(
+            res => {
+                console.log('Questions List: ' + JSON.stringify(res));
+                this.categories = res["categories"];
+                this.initializeScore();
+            },
+            err => {
+                console.log('Error reading json: ' + JSON.stringify(err));
+            }
+        )
     }
 
     initializeScore() {
